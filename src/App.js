5848d4757e6275fd56b14cd0591f9b4b4c1542f5
Esteban Aguirre Martinez <estebanaguirre1997@gmail.com>
2020-12-03 11:26:02 -0500

    add elementos deploy

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component{
       );
       return (
         <div className="App">
-          
+          <h1> Monsters Rolodex </h1>
           <SearchBox placeholder = 'search monsters'
           handleChange = {this.handleChange}/>
           <CardList monsters = {filteredMonster} >
